e01f196225fcf572f2449da66347866c11936079
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-10-10 12:02:50 +0300

    Complete Anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+# anagram_list = ['enlists', 'google', 'inlets', 'banana']
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+
+    def match(self,anagram_list):
+        anagram_word = ''.join(sorted(self.word))
+        sorted_list = [''.join(sorted(lett)) for lett in anagram_list]
+        matches = []
+        for word in anagram_list:
+            if ''.join(sorted(word)) in sorted_list and ''.join(sorted(word)) == anagram_word:
+                matches.append(word)
+        return matches 
+
+    
+# ana = Anagram("lisen")
+# print(ana.match(anagram_list))
